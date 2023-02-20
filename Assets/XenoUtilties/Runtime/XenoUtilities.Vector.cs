8e214072b1665e793e43dc26392e562f79f6e30b
songqin <980047129@qq.com>
2023-02-20 11:03:16 +0800

    update

--- Assets/XenoUtilties/Runtime/XenoUtilities.Vector.cs
+++ Assets/XenoUtilties/Runtime/XenoUtilities.Vector.cs
@@ -7,45 +7,22 @@ namespace Xeno.Utilities
      */
     public static partial class XenoUtilities
     {
-        public static Vector2 Vec2xy(this Vector3 vector)
-        {
-            return new Vector2(vector.x, vector.y);
-        }
+        public static Vector2 Vec2xy(this Vector3 vector) => new Vector2(vector.x, vector.y);
         
-        public static Vector2 Vec2xz(this Vector3 vector)
-        {
-            return new Vector2(vector.x, vector.z);
-        }
+        public static Vector2 Vec2xz(this Vector3 vector) => new Vector2(vector.x, vector.z);
         
-        public static Vector2 Vec2yz(this Vector3 vector)
-        {
-            return new Vector2(vector.y, vector.z);
-        }
+        public static Vector2 Vec2yz(this Vector3 vector) => new Vector2(vector.y, vector.z);
         
-        public static Vector3 Vec3xy(this Vector2 vector)
-        {
-            return new Vector3(vector.x, vector.y, 0);
-        }
+        public static Vector3 Vec3xy(this Vector2 vector) => new Vector3(vector.x, vector.y, 0);
         
-        public static Vector3 Vec3xz(this Vector2 vector)
-        {
-            return new Vector3(vector.x, 0, vector.y);
-        }
+        public static Vector3 Vec3xz(this Vector2 vector) => new Vector3(vector.x, 0, vector.y);
         
-        public static Vector3 Vec3xy(this Vector3 vector)
-        {
-            return new Vector3(vector.x, vector.y, 0);
-        }
+        public static Vector3 Vec3xy(this Vector3 vector) => new Vector3(vector.x, vector.y, 0);
         
-        public static Vector3 Vec3xz(this Vector3 vector)
-        {
-            return new Vector3(vector.x, 0, vector.z);
-        }
+        public static Vector3 Vec3xz(this Vector3 vector) => new Vector3(vector.x, 0, vector.z);
         
-        public static Vector3 Vec3yz(this Vector3 vector)
-        {
-            return new Vector3(0, vector.y, vector.z);
-        }
+        public static Vector3 Vec3yz(this Vector3 vector) => new Vector3(0, vector.y, vector.z);
+
 
     }
 }
--- Assets/XenoUtilties/Test/Test.cs
+++ Assets/XenoUtilties/Test/Test.cs
@@ -8,7 +8,6 @@ namespace Xeno.Utilties.Test
         public void Main()
         {
             Vector3 a = new Vector3(1, 1, 1);
-            Vector2 b = a.Vec2xz();
         }
     }
 }

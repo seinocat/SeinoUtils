0b13133515878719587f996762f194c6705de285
songqin <980047129@qq.com>
2023-08-09 17:50:56 +0800

    添加批处理

---
+++ Action_UPM_Rebuild.bat
@@ -0,0 +1,2 @@
+git subtree split --prefix=Assets/XenoUtilities --branch upm
+git push origin upm
--- Assets/XenoUtilities/Runtime/XenoUtilities.List.cs
+++ Assets/XenoUtilities/Runtime/XenoUtilities.List.cs
@@ -5,13 +5,10 @@ namespace Xeno.Utilities
 {
     public static partial class XenoUtilities
     {
-
         public static T GetLast<T>(this List<T> list)
         {
             return list[list.Count - 1];
         }
-
-        
         
     }
 }

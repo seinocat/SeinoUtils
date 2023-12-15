12837a9b1d07444db44847fdf0e44a7f0109f398
songqin <980047129@qq.com>
2023-12-15 09:45:36 +0800

    feat:增加单例

---
+++ Runtime/Core/Tick/Ticker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Seino.Utils.Singleton;
+
+namespace Seino.Utils.Tick
+{
+    public class Ticker : MonoSingleton<Ticker>
+    {
+        private void Update()
+        {
+            
+        }
+
+        public TickChannel CreateChannel<T>(IList<T> dataList, Action executor) where T : class
+        {
+            TickChannel channel = new TickChannel();
+            
+        }
+        
+        // public TickChannel CreateChannel<T1, T2>(IDictionary<T1, T2> dataList, Func<T1, T2> executer) where T2 : class
+        // {
+        //     Dictionary<>
+        // }
+    }
+}

a5e79317383b02aab25b14afaeeccd769e41319f
songqin <980047129@qq.com>
2023-12-15 09:56:41 +0800

    fix:编译错误

--- Unity/Assets/SeinoUtils/Runtime/Core/Tick/TickChannel.cs
+++ Unity/Assets/SeinoUtils/Runtime/Core/Tick/TickChannel.cs
@@ -1,7 +1,26 @@
-﻿namespace Seino.Utils.Tick
+﻿using System;
+
+namespace Seino.Utils.Tick
 {
     public class TickChannel
     {
-        
+        public int FrameRate = 1;
+
+        private float m_IntervalTime;
+        private float m_Time;
+        public Action Executor;
+
+        public TickChannel()
+        {
+            m_IntervalTime = 1f / FrameRate;
+        }
+
+        public void Update(float deltaTime)
+        {
+            m_Time += deltaTime;
+            if (m_Time < m_IntervalTime) return;
+            m_Time = 0;
+            Executor();
+        }
     }
 }

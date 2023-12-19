918cd091939d04eb34a0fbac52a7f2992f097977
songqin <980047129@qq.com>
2023-12-19 15:44:40 +0800

    feat:增加ticker执行器

--- Runtime/Core/Tick/TickChannel.cs
+++ Runtime/Core/Tick/TickChannel.cs
@@ -4,23 +4,52 @@ namespace Seino.Utils.Tick
 {
     public class TickChannel
     {
-        public int FrameRate = 1;
+        public long Id => m_id;
+        public bool IsPause;
 
         private float m_IntervalTime;
         private float m_Time;
-        public Action Executor;
+        private long m_id;
+        private int m_frame = 1;
+        private Action m_executor;
+        private Func<bool> m_predicate;
+        private Action m_callback;
+
+
+        public static TickChannel Create(long id, Func<bool> pre, Action exe, Action call, int frame)
+        {
+            TickChannel channel = new TickChannel();
+            channel.m_id = id;
+            channel.m_executor = exe;
+            channel.m_predicate = pre;
+            channel.m_callback = call;
+            channel.m_frame = frame;
+            return channel;
+        }
 
         public TickChannel()
         {
-            m_IntervalTime = 1f / FrameRate;
+            m_IntervalTime = 1f / m_frame;
         }
 
         public void Update(float deltaTime)
         {
             m_Time += deltaTime;
-            if (m_Time < m_IntervalTime) return;
-            m_Time = 0;
-            Executor();
+            if (m_Time >= m_IntervalTime)
+            {
+                m_Time = 0;
+                m_executor();
+                if (m_predicate?.Invoke()??false)
+                {
+                    m_callback?.Invoke();
+                    Dispose();
+                } 
+            }
+        }
+
+        public void Dispose()
+        {
+            SeinoTicker.Instance.Remove(Id);
         }
     }
 }

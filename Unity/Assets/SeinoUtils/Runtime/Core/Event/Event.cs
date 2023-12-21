74eca5a49145a1906845c583b7751128952178e5
songqin <980047129@qq.com>
2023-12-21 17:22:55 +0800

    feat:增加events

---
+++ Unity/Assets/SeinoUtils/Runtime/Core/Event/Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SeinoUtils.Runtime.Core.Event
+{
+    abstract class Event 
+    {
+        public abstract void Call(object message);
+    }
+    
+    class Event<T> : Event where T : class
+    {
+        public Action<T> handler;
+        
+        public void AddListener(Action<T> listener)
+        {
+            handler += listener;
+        }
+
+        public void RemoveListener(Action<T> listener)
+        {
+            handler -= listener;
+        }
+
+        public override void Call(object message)
+        {
+            if(handler != null)
+                handler.Invoke(message as T);
+        }
+    }
+}

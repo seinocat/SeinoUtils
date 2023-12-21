b1f4427355e975701a34b9cbcb44f745a0a1f5c8
songqin <980047129@qq.com>
2023-12-21 11:21:35 +0800

    chore:调整vector

--- Unity/Assets/SeinoUtils/Runtime/Core/Utils/SeinoUtils.Vector.cs
+++ Unity/Assets/SeinoUtils/Runtime/Core/Utils/SeinoUtils.Vector.cs
@@ -15,13 +15,17 @@ namespace Seino.Utils
         
         public static Vector3 Vec3xy(this Vector2 vector) => new Vector3(vector.x, vector.y, 0);
         
+        public static Vector3 Vec3yz(this Vector2 vector) => new Vector3(0, vector.x, vector.y);
+        
         public static Vector3 Vec3xz(this Vector2 vector) => new Vector3(vector.x, 0, vector.y);
         
         public static Vector3 Vec3xy(this Vector3 vector) => new Vector3(vector.x, vector.y, 0);
         
+        public static Vector3 Vec3yz(this Vector3 vector) => new Vector3(0, vector.y, vector.z);
+        
         public static Vector3 Vec3xz(this Vector3 vector) => new Vector3(vector.x, 0, vector.z);
         
-        public static Vector3 Vec3yz(this Vector3 vector) => new Vector3(0, vector.y, vector.z);
+        
         
     }
 }

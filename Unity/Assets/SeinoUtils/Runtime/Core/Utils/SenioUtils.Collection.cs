a4a917bf5b96d7049f4178a3e8380fd55d111424
songqin <980047129@qq.com>
2024-03-19 15:12:41 +0800

    feat:移动元素方法

---
+++ Unity/Assets/SeinoUtils/Runtime/Core/Utils/SenioUtils.Collection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Seino.Utils
+{
+    public static partial class SenioUtils
+    {
+        private static void MoveElement<T>(ref T[] array, int sourceIndex, int targetIndex)
+        {
+            if (sourceIndex < 0 || sourceIndex >= array.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(sourceIndex), "Source index is out of range.");
+            }
+
+            if (targetIndex < 0 || targetIndex >= array.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(targetIndex), "Target index is out of range.");
+            }
+
+            // 保存要移动的元素
+            T elementToMove = array[sourceIndex];
+
+            // 移动元素
+            if (sourceIndex < targetIndex)
+            {
+                for (int i = sourceIndex; i < targetIndex; i++)
+                {
+                    array[i] = array[i + 1];
+                }
+            }
+            else if (sourceIndex > targetIndex)
+            {
+                for (int i = sourceIndex; i > targetIndex; i--)
+                {
+                    array[i] = array[i - 1];
+                }
+            }
+
+            // 将元素放入目标位置
+            array[targetIndex] = elementToMove;
+        }
+    }
+}
